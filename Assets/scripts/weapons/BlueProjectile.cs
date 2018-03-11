9dfecf03fcb5cce049b4573fb388cddc49da6d66
eric-zdw <eric.zdw@gmail.com>
2018-03-11 01:20:21 -0500

    Blue weapon

--- Assets/scripts/weapons/BlueProjectile.cs
+++ Assets/scripts/weapons/BlueProjectile.cs
@@ -5,66 +5,98 @@ using UnityEngine;
 public class BlueProjectile : Projectile {
 
     public GameObject explosion;
-    public GameObject explosion2;
+    public GameObject hitbox;
 
-    private float damageDecayRate;
-    private BoxCollider collider;
+    public float extraGravityForce;
+    public float launchForce;
+
+    private Rigidbody rb;
+    private CameraFollow camfollow;
+	private Camera cam;
+
+    private float radius;
+
+	private Vector3 direction;
+	private GameObject player;
+	private Vector3 mousePosition;
 
     // Use this for initialization
     void Start() {
-        projectileSpeed = 2f;
-        lifeTime = 0.5f;
-        collider = GetComponent<BoxCollider>();
+        lifeTime = 8f;
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.right * launchForce);
+		cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         if (lifeTime <= 0)
-            Destroy(gameObject);
+            Explode();
         else
         {
-            Propogate();
             lifeTime -= Time.deltaTime;
         }
 
-        damage -= damageDecayRate * Time.deltaTime;
-        projectileSpeed *= 1.25f;
-        transform.localScale = new Vector3(transform.localScale.x * 1.2f, transform.localScale.y, transform.localScale.z);
+		CheckDetonate();
+        rb.AddForce(new Vector3(0, -extraGravityForce, 0) * Time.deltaTime);
+
+		if (Input.GetButton("Fire1"))
+			rb.AddForce(Vector3.Normalize((player.transform.position) - (transform.position)) * Time.deltaTime * 4000f);
+
+		if (Input.GetButtonUp("Fire1")) {
+			rb.velocity = new Vector3(0, 0, 0);
+			mousePosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 11f));
+			rb.AddForce(Vector3.Normalize(mousePosition - (transform.position)) * 2000f);
+			Destroy(gameObject, 1f);
+		}
+
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+		if (other.CompareTag("Enemy"))
         {
-            Vector3 expPosition = collider.ClosestPointOnBounds(other.transform.position);
-            Instantiate(explosion, expPosition, transform.rotation);
-            //Instantiate(explosion2, expPosition, transform.rotation);
-            other.gameObject.GetComponent<Enemy>().getDamage(damage);
-            print("real damage: " + damage);
-            damage *= 0.6f;
-        }
-        else if (!other.gameObject.CompareTag("Player"))
-        {
-            //Vector3 expPosition = collider.ClosestPointOnBounds(other.transform.position);
-            //Instantiate(explosion, expPosition, transform.rotation);
-            //Instantiate(explosion2, expPosition, transform.rotation);
-            //damage *= 0.5f;
-            Destroy(gameObject, 0.1f);
+			Instantiate(explosion, transform.position, transform.rotation);
+			//other.GetComponent<Rigidbody>().AddExplosionForce(500f, transform.position, radius * 2);
+			other.gameObject.GetComponent<Enemy>().getDamage(damage);
+			Destroy(gameObject);
         }
     }
 
     public void setDamage(float d)
     {
         damage = d;
-        //damageDecayRate = damage * 0.5f;
+    }
+
+    public void setRadius(float r)
+    {
+        radius = r;
     }
 
     void Explode()
     {
-        Instantiate(explosion, transform.position, transform.rotation);
-        Instantiate(explosion2, transform.position, transform.rotation);
+
+        GameObject exp = Instantiate(explosion, transform.position, transform.rotation);
+        exp.transform.localScale = new Vector3(radius * 0.5f, radius * 0.5f, radius * 0.5f);
+
+        OrangeProjectileHitbox hb = Instantiate(hitbox, transform.position, Quaternion.identity).GetComponent<OrangeProjectileHitbox>();
+        hb.GetComponent<SphereCollider>().radius = radius;
+        hb.setDamage(damage);
+        hb.setRadius(radius / 0.5f);
+        hb.printRadius();
+
         Destroy(gameObject);
     }
+
+	void CheckDetonate()
+	{
+		if (Input.GetButtonDown("Fire2"))
+		{
+			Explode();
+		}
+	}
 
 
 }
---
+++ Assets/scripts/weapons/BlueWeapon.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueWeapon : Weapon {
+
+    public GameObject projectile;
+    private Camera cam;
+    private PlayerBehaviour player;
+    private Vector2 mousePosition;
+    private float angle;
+    public float bFireRate = 0.2f;
+
+    public float heatDamageRate = 0.005f;
+    public float heatFireRate = 0.004f;
+    public float damage = 6f;
+
+    void Start () {
+        SetFireRate(bFireRate);
+        WeaponType weaponType = WeaponType.Automatic;
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+    }
+
+    void Update()
+    {
+        if (GetCooldown() > 0)
+            DecrementCooldown();
+
+        mousePosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 11f));
+        angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+    }
+
+    public override void Fire1()
+    {
+        print(
+            "Current heat factor: " + player.getHeatFactor()
+            + " , damage: " + damage * (1f + (heatDamageRate * player.getHeatFactor()))
+            + " , fire rate: " + bFireRate / (1f + (heatFireRate * player.getHeatFactor())));
+        if (GetCooldown() <= 0)
+        {
+            float realDamage = damage * (1f + (heatDamageRate * player.getHeatFactor()));
+            GameObject proj = Instantiate(
+                projectile, 
+                transform.position + (Vector3.Normalize((Vector3)mousePosition - transform.position) * 0.5f), 
+                Quaternion.Euler(0, 0, angle)
+                );
+
+
+            proj.GetComponent<BlueProjectile>().setDamage(realDamage);
+
+            SetCooldown(bFireRate / (1f + (heatFireRate * player.getHeatFactor())));
+        }
+    }
+
+    public override void Fire2()
+    {
+    }
+}
