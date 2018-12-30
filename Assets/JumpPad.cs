74e67f62c05d215d3bce6877ab184d47be589f2c
Eric Du <16ezd@queensu.ca>
2018-12-30 01:57:46 -0500

    Main menu changes

--- Assets/JumpPad.cs
+++ Assets/JumpPad.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour {
 
     public float boost;
+    public float boostCooldown = 0.1f;
+    private float currentBoostCooldown;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +15,19 @@ public class JumpPad : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (currentBoostCooldown > 0f)
+        {
+            currentBoostCooldown -= Time.deltaTime;
+        }
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && currentBoostCooldown <= 0f)
         {
             Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - boost, rb.velocity.z);
+            currentBoostCooldown = boostCooldown;
         }
     }
 }
