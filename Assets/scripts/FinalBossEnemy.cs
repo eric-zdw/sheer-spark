dc3743672c472b6a7abe33c777f3c2a163fd7593
Eric Du <16ezd@queensu.ca>
2019-09-12 15:31:53 -0400

    Add Boss Enemy + Add BossEnemy visuals

---
+++ Assets/scripts/FinalBossEnemy.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossEnemy : Enemy {
+	public GameObject healthBar;
+	public float newHealth;
+	private GameObject bar;
+
+	private GameObject player;
+	private Vector3 playerLocation;
+	private Rigidbody rb;
+
+	public GameObject[] powerups;
+    public Material[] colours;
+	public Material[] seeThroughs;
+	public GameObject[] explosions;
+	public Material damagedMaterial;
+    public float YLimit;
+	private MeshRenderer damageFlash;
+    private MeshRenderer outline;
+	private MeshRenderer seeThrough;
+    private int powerupRoll;
+
+	private NoiseManager noiseManager;
+
+    public bool isTethered;
+    private float tetheredCheck = 0.05f;
+    private float tetheredTimer;
+
+	private MaterialPropertyBlock damageMatBlock;
+
+	private int numberOfAttacks = 5;
+
+    void Start()
+	{
+		bar = Instantiate(healthBar);
+		maxHealth = newHealth;
+		health = maxHealth;
+		bar.GetComponent<HealthBar>().setTarget(gameObject);
+
+		player = GameObject.FindGameObjectWithTag("Player");
+		rb = GetComponent<Rigidbody>();
+		noiseManager = GameObject.FindGameObjectWithTag("PlayerCam").GetComponent<NoiseManager>();
+
+		powerupRoll = Random.Range(0, 6);
+		damageFlash = transform.GetChild(2).GetComponent<MeshRenderer>();
+        outline = transform.GetChild(0).GetComponent<MeshRenderer>();
+		seeThrough = transform.GetChild(1).GetComponent<MeshRenderer>();
+        outline.material = colours[powerupRoll];
+		seeThrough.material = seeThroughs[powerupRoll];
+
+		damageMatBlock = new MaterialPropertyBlock();
+		StartCoroutine(BossAttackRoutine());
+    }
+
+	void FixedUpdate()
+	{
+		if (health <= 0)
+		{
+			Explode();
+		}
+
+		rb.AddForce(Vector3.Normalize(player.transform.position - transform.position) * 1800f * Time.deltaTime);
+
+		//rb.AddForce(new Vector3(-rb.velocity.x, 0, 0) * 2f * Time.deltaTime);
+		//rb.AddForce(new Vector3(0, -rb.velocity.y, 0) * 80f * Time.deltaTime);
+	    
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().takeDamage(1);
+            Explode();
+        }
+    }
+
+    void Explode()
+	{
+		noiseManager.AddNoise(10f);
+		Instantiate(powerups[powerupRoll], transform.position, Quaternion.identity);
+		Instantiate(explosions[powerupRoll], transform.position, transform.rotation);
+		Destroy(bar);
+		Destroy(gameObject);
+	}
+
+	public override void getDamage(float damage)
+    {
+        health -= damage;
+		StartCoroutine(FlashWhite());
+    }
+
+	IEnumerator FlashWhite() {
+		float colorValue = 2f;
+		Color newColor = new Color(colorValue, colorValue, colorValue, 1);
+		while (colorValue > 0f) {
+			colorValue -= 5f * Time.deltaTime;
+			newColor = new Color(colorValue, colorValue, colorValue, 1);
+			damageMatBlock.SetColor("_EmissionColor", newColor);
+			damageFlash.SetPropertyBlock(damageMatBlock);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	IEnumerator BossAttackRoutine() {
+		while (true) {
+			//Wait period
+			yield return new WaitForSeconds(5f);
+
+			//Attack period
+			int chooseAttack = Random.Range(1, numberOfAttacks+1);
+			
+		}
+	}
+}
