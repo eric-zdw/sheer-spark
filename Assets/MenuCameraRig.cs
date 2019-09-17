29a01aa5fbe9e85f8ed1100c5dcab189093862f0
Eric Du <16ezd@queensu.ca>
2019-09-17 16:11:18 -0400

    Add menu transition system

--- Assets/MenuCameraRig.cs
+++ Assets/MenuCameraRig.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class MenuCameraRig : MonoBehaviour {
 
+	public Vector3[] positions;
+	public Vector3[] rotations;
+	public static int currentPosition = 0;
+	public float transitionRate = 0.05f;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		transform.position = Vector3.Lerp(transform.position, positions[currentPosition], transitionRate);
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotations[currentPosition]), transitionRate);
+	}
+
+	public static void ChangePosition(int index) {
+		currentPosition = index;
 	}
 }
---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+
+	public GameObject mainMenuScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			MenuCameraRig.ChangePosition(1);
+			mainMenuScreen.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
